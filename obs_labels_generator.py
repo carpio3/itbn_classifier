b349b8a1fcf51efe42c467fd7742750536b3148f
Estuardo Carpio <eccarpio@hotmail.com>
2018-01-03 16:52:50 -0600

    test

--- obs_labels_generator.py
+++ obs_labels_generator.py
@@ -168,6 +168,7 @@ if __name__ == '__main__':
     while len(filenames) > 0:
         # read a batch of tfrecords into np arrays
         with aud_dqn.sess.as_default():
+            with aud_dqn.graph.as_default():
             seq_len, opt_raw, aud_raw, timing_labels, timing_values, name = aud_dqn.sess.run(
                 [seq_len_inp, opt_raw_inp, aud_raw_inp, timing_labels_inp, timing_values_inp, name_inp])
 
